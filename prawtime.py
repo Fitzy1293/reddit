9e5cb1024e76f1c8e03ae5f6bb9b8723aaaa4606
Owen Fitzgerald <42453847+Fitzy1293@users.noreply.github.com>
2019-02-26 02:25:33 -0500

    Create prawtime.py

---
+++ prawtime.py
@@ -0,0 +1,39 @@
+import praw
+import time
+import datetime
+from datetime import datetime, timedelta
+
+#reddit = praw.Reddit('Your authentification info.')
+
+def main():
+    subreddit = input('Enter a subreddit >> ')
+    print()
+    validSubmissions = submissionsWithin24hours(subreddit)
+
+    for submission in validSubmissions:
+        try:
+            print(*submission,sep='\n')
+            print()
+        except UnicodeEncodeError:
+                continue
+
+def submissionsWithin24hours(subreddit):
+    subreddit = reddit.subreddit(subreddit)
+
+    submissionsLast24 = []
+    for submission in subreddit.hot(limit=None):
+        utcPostTime = submission.created
+        submissionDate = datetime.utcfromtimestamp(utcPostTime)        
+        currentTime = datetime.utcnow()
+        
+        submissionDelta = currentTime - submissionDate #How long ago it was posted.
+        
+        title = submission.title
+        link = 'www.reddit.com' + submission.permalink
+        #if 'day' not in str(submissionDelta):
+        submissionsLast24.append((title, link, submissionDelta))
+  
+    return submissionsLast24
+
+main()
+
