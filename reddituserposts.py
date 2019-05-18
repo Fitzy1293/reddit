4c79d73e25b088dfa53384e1a17d7f3e0de08d62
Owen Fitzgerald <42453847+Fitzy1293@users.noreply.github.com>
2019-05-18 16:55:08 -0400

    Update reddituserposts.py

--- reddituserposts.py
+++ reddituserposts.py
@@ -1,43 +1,10 @@
 #The purpose of this program is to get a list of every subreddit a user has posted in, and how often.
 #From this list, you can pick a subreddit to get all of their comments and posts from.
-#The output is a text file and a histogram of all of their posts for each subreddit. 
 
 import praw
+from authenticate import authenticate
 import pprint, collections, os, sys
 from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-
-def main():
-    reddit = praw.Reddit(client_id='Your client id',
-                     client_secret='Your client secret',
-                     user_agent='reddit /u/your username, https://github.com/Fitzy1293/reddituserposts')               
-    
-    userstr = input('Enter a username >> ')
-    user = reddit.redditor(userstr)
-
-    #Prints comments and submissions. 
-    print('Comments:')
-    for commentCount in userCommentCount(user):
-        print('r/' + commentCount[0] + ': ' + str(commentCount[1]) + ' comment(s). ')
-    print('=' * 134)
-    print('Submissions:')
-    for submissionCount in userSubmissionCount(user):
-        print('r/' + submissionCount[0] + ': ' + str(submissionCount[1]) + ' submission(s). ')
-
-    #For a specifc subreddit.
-    userSubredditstr = input('\nEnter a subreddit to see all of ' + str(user) + '\'s posts in that subreddit. r/')
-    userSubreddit = reddit.subreddit(userSubredditstr)
-
-    comments = subredditComments(user, userSubreddit)
-    submissions = subredditSubmissions(user, userSubreddit)
-
-    #Creates .txt file and graph. 
-    txtExport(userstr + '.txt', comments, submissions)
-    exportPath = os.path.join(os.getcwd(), userstr + '.txt')
-    print(exportPath)
-    os.startfile(exportPath, 'open')
-    plotSubreddits(userstr, userSubmissionCount(user), userCommentCount(user))
 
 #Returns # of comments by user as a list of (subreddit, comment count).
 def userCommentCount(user):
@@ -110,47 +77,59 @@ def subredditSubmissions(user, userSubreddit):
 def txtExport(filename, comments, submissions):
     txt = open(filename + '.','w', encoding = 'utf8')
 
-    txt.write('Comments - \n\n')
-    for i, comment in enumerate(comments):
-        txt.write('Comment ' + str(i+1) + ': ' + comment[0] + '\n')
-        txt.write(comment[1] + '\n\n')
-        txt.write(comment[2] + '\n\n')
+    if len(comments)!=0:
+        txt.write('Comments - \n\n')
+        for i, comment in enumerate(comments):
+            txt.write('Comment ' + str(i+1) + ': ' + comment[0] + '\n')
+            txt.write(comment[1] + '\n\n')
+            txt.write(comment[2] + '\n\n')
 
-
-    txt.write('Submissions - \n\n')
-    for i, submission in enumerate(submissions):
-        txt.write('Submission ' + str(i+1) + ': ' + submission[0] + '\n')
-        txt.write(submission[1] + '\n\n')
+    if len(submissions)!=0:
+        txt.write('Submissions - \n\n')
+        for i, submission in enumerate(submissions):
+            txt.write('Submission ' + str(i+1) + ': ' + submission[0] + '\n')
+            txt.write(submission[1] + '\n\n')
     
     txt.close()
-    return txt
 
-#Creates plot of total submissions and comments for each subreddit. 
-def plotSubreddits(userstr, userCommentCount, userSubmissionCount):
-    #Dictionary for total number of posts to every subreddit - posts can either be comments or submissions. 
+def main(reddit):
+    
+    userstr = input('Enter a username >> ')
+    user = reddit.redditor(userstr)
+    print()
 
-    allPosts = userCommentCount + userSubmissionCount
-    allPostsDict = {}
-    for post in allPosts:
-        subreddit = post[0]
-        count = post[1]
-        
-        if subreddit not in allPostsDict.keys():
-            allPostsDict[subreddit] = count
-        else:
-            allPostsDict[subreddit] = count + allPostsDict[subreddit]
-   
-    plt.bar(allPostsDict.keys(),allPostsDict.values(), color = 'red', width = .25)
-    plt.xticks(rotation = 90, fontsize = 8)
-    plt.subplots_adjust(bottom = .25)
+    #Prints comments and submissions.
+    commentCounts = userCommentCount(user)
+    print('Comments:')
+    for commentCount in commentCounts:
+        print('r/' + commentCount[0] + ': ' + str(commentCount[1]) + ' comment(s). ')
 
-    plt.title(userstr + '\'s posts')
-    plt.xlabel('Subreddits')
-    plt.ylabel('Comments and submissions')
-    plt.show()
+    print()
 
-main()
+    submissionCounts = userSubmissionCount(user)
+    print('Submissions:')
+    for submissionCount in submissionCounts:
+        print('r/' + submissionCount[0] + ': ' + str(submissionCount[1]) + ' submission(s). ')
 
+    #For a specifc subreddit.
+    userSubredditstr = input('\nEnter a subreddit to see all of ' + str(user) + '\'s posts in that subreddit. \nr/')
+    print()
+    userSubreddit = reddit.subreddit(userSubredditstr)
 
+    comments = subredditComments(user, userSubreddit)
+    submissions = subredditSubmissions(user, userSubreddit)
 
+    if len(comments) + len(submissions) != 0: #If they've actually submitted to that sub. 
+        #Creates .txt file. 
+        txtExport(userstr + '_' + userSubredditstr + '.txt', comments, submissions)
+        exportPath = os.path.join(os.getcwd(), userstr + '_' + userSubredditstr + '.txt')
+        print(exportPath)
+        os.startfile(exportPath, 'open')
 
+    else:
+        print('\n' + str(user) + ' has not submitted to ' + str(userSubreddit) + ' in their last 1k posts.')
+
+reddit = authenticate()                                                                       
+while True:
+    main(reddit)
+    print()
