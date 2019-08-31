101d764364decbe6835a14558cd562aa0fffae85
Owen Fitzgerald <42453847+Fitzy1293@users.noreply.github.com>
2019-08-30 20:22:57 -0400

    Update subsBySubscribers.py

--- subsBySubscribers.py
+++ subsBySubscribers.py
@@ -3,28 +3,24 @@ import requests
 from pprint import pprint
 import os
 
-
-
-def getInfo(url):
+def getSubs(url):
     page = requests.get(url)
     htmlContent = html.fromstring(page.content)
 
-    body = htmlContent[1]
+    body = htmlContent[1] 
     body = etree.tostring(body)
-    body = body.decode('UTF-8')
-
-
+    body = body.decode('UTF-8') #Decode bitecode into a string.
     body = body.split('\n')
-    body = [line for line in body if '<td class="tod"' in line]
+    body = [line for line in body if '<td class="tod"' in line] #'<td class="tod"' is where the info about the subs is. 
 
-    subredditInfo = []
+    subredditInfo = [] #2D list, 3 attributes in inner lists - Rank, Subreddit URL and sub count.
     for i in range(0, len(body), 3):
         subredditInfo.append(body[i:i+3])  
 
     formatSubreddit = []
     for subreddit in subredditInfo:
         formatAttribute = []
-        for j, attribute in enumerate(subreddit):
+        for j, attribute in enumerate(subreddit): #Probably not the correct way to parse this but whatever.
             if j==0:
                 rank = attribute.split('>')[-2]
                 rank = rank.split('<')[0]
@@ -41,26 +37,25 @@ def getInfo(url):
                 subscribers = subscribers.split('>')[-1]
                 formatAttribute.append('\t' + subscribers)
                 
-
         formatSubreddit.append(''.join(formatAttribute))
 
     return formatSubreddit
             
 def main():
-    path = 'C:\\Users\\Owen\\Documents\\Python Scripts\\reddit\\subs.txt'
+    path = 'C:\\Users\\You (YOU IS NOT MY REAL NAME)\\Documents\\Python Scripts\\reddit\\subs.txt'
     f = open(path, 'w')
-    top100 = getInfo('https://redditmetrics.com/top')
     
+    top100 = getSubs('https://redditmetrics.com/top') #First 100.
     for subreddit in top100:
         f.write(subreddit + '\n')
     
-    url = 'https://redditmetrics.com/top/offset/'
-    for i in range(100, 2000000, 100):
-        try:
-            subreddits = getInfo(url + str(i))
+    url = 'https://redditmetrics.com/top/offset/' #Rest of them.
+    for i in range(100, 2000000, 100): #Larger range than there are number of subs.
+        try:                           #Will get an exception when it tries to reach a webpage for subs that don't exist and break.
+            subreddits = getSubs(url + str(i))
             for subreddit in subreddits:
                 f.write(subreddit + '\n')
-        except:
+        except: 
             break
             
     f.close()
