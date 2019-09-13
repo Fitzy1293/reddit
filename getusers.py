5c5ffbdbe3c7fae26661fb3078803394392dcb2b
Owen Fitzgerald <42453847+Fitzy1293@users.noreply.github.com>
2019-09-13 16:37:46 -0400

    Update getusers.py

--- getusers.py
+++ getusers.py
@@ -1,13 +1,15 @@
 import praw
 import prawcore
-from authenticate import authenticate
-from pprint import pprint
+from authenticate import authenticate #Module containing PRAW authentification info
+from pprint import pprint             #Put this in the same folder as as this program, or just include it in this module.
 import os
 
-reddit = authenticate() 
-directory = 'C:\\Users\\Owen\\Documents\\Python Scripts\\reddit'
+reddit = authenticate() #Authenticating reddit, you do can just authenticate it the normal way.
+                        #I do this so I don't have to change anything when I update it. 
 
-subreddits = open(os.path.join(directory,'topsubs.txt'), 'r').read().splitlines()
+directory = 'C:\\Users\\' #Directory you want users.txt created in. 
+
+subreddits = open(os.path.join(directory,'topsubs.txt'), 'r').read().splitlines() #Use the topsubs.txt in my reddit repository.
 subreddits = [subreddit.split('/')[-1] for subreddit in subreddits if ('/') in subreddit]
 
 userNames = []
@@ -23,6 +25,7 @@ for i, subreddit in enumerate(subreddits):
 
 userNames = set([userName for userName in userNames])
 print(len(userNames))
+
 users = open(os.path.join(directory,'users.txt'), 'w')
 for name in userNames:
     users.write(name + '\n')
