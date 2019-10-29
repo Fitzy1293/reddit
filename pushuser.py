2b0fc52ba0fc9325c49db2eee4fe2749b10f8db5
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2019-10-29 10:12:31 -0400

    Add files via upload

---
+++ pushuser.py
@@ -0,0 +1,123 @@
+import urllib.request,json, time, os
+from pprint import pprint
+from datetime import datetime
+
+def getComments(user):
+    author = user
+    url = f'https://api.pushshift.io/reddit/search/comment/?author={author}&size=1000'
+    
+    IDs = []
+    ct = 0
+    while True:
+        req = urllib.request.urlopen(url)
+        response =  json.loads(req.read())
+        data = response['data']
+
+        onekIDs = []
+        for i in data:
+            commentDict = ({'ID': i['id'],
+                            'type': 'comment',
+                            'subreddit': i['subreddit'],
+                            'utc': i['created_utc'],
+                            'date': str(datetime.utcfromtimestamp(i['created_utc'])),
+                            'body': i['body']})
+                            
+            
+            if 'permalink' in i.keys():
+                commentDict['link'] = f'https://www.reddit.com + i["permalink"]'
+            
+            onekIDs.append(commentDict)
+    
+        IDs.append(onekIDs)
+        if len(onekIDs)==0:
+            break
+        
+        print(url)
+        print(f'Comment list #{ct+1}, containing info on {len(onekIDs)} comments is complete.')
+        print()
+        
+        newBefore = onekIDs[-1]['utc']
+        url = f'https://api.pushshift.io/reddit/search/comment/?author={author}&size=1000&before={newBefore}'
+        ct = ct+1
+
+    postDict = [ID for IDSet in IDs for ID in IDSet]
+    postDict.reverse()    
+    
+    return postDict
+
+def getSubmissions(user):
+    author = user
+    url = f'https://api.pushshift.io/reddit/search/submission/?author={author}&size=1000'
+    
+    IDs = []
+    ct = 0
+    while True:
+        req = urllib.request.urlopen(url)
+        response =  json.loads(req.read())
+        data = response['data']
+
+        onekIDs = []
+        for i in data:
+            submissionDict = ({'ID': i['id'],
+                               'type': 'submission',
+                               'subreddit': i['subreddit'],
+                               'utc': i['created_utc'],
+                               'date': str(datetime.utcfromtimestamp(i['created_utc']))})
+            
+            if 'permalink' in i.keys():
+                submissionDict['link'] = f'https://www.reddit.com + i["permalink"]' 
+
+            onekIDs.append(submissionDict)
+    
+        IDs.append(onekIDs)
+
+        if len(onekIDs)==0:
+            break
+        
+        print(url)
+        print(f'Submission list #{ct+1}, containing info on {len(onekIDs)} submissions is complete.')
+        print()
+        
+        newBefore = onekIDs[-1]['utc']
+        url = f'https://api.pushshift.io/reddit/search/submission/?author={author}&size=1000&before={newBefore}'
+        ct = ct+1
+
+    postDict = [ID for IDSet in IDs for ID in IDSet]
+    postDict.reverse()    
+    
+    return postDict
+
+def writeFiles(postDict, user):
+    if len(postDict)!=0:
+        earliestPost = postDict[-1]['utc']
+        latestPost = postDict[0]['utc']
+
+        jname = f'{user}_posts.json'
+        with open(jname, 'w+') as f:
+            json.dump(postDict, f, indent=4, sort_keys=True)
+
+        print(f'{len(postDict)} IDs retrieved.')
+
+        os.startfile(jname)
+
+
+def main():
+    user = input('Enter a user >> ')
+    start = time.time()
+    print('Gathering comments.\n')
+    comments = getComments(user)
+    print('\nGathering submissions.')
+    submissions = getSubmissions(user)
+
+    postDic = submissions + comments
+    
+    
+    writeFiles(postDic, user)
+    end = time.time()
+
+    print(f'\n{end-start} seconds to run.')
+        
+main()
+            
+
+
