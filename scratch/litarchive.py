fd46eb9df3ed0931fecc16f18fbfe993249b311a
Fitzy1293 <42453847+Fitzy1293@users.noreply.github.com>
2019-10-31 08:16:51 -0400

    Update litarchive.py

--- scratch/litarchive.py
+++ scratch/litarchive.py
@@ -1,49 +1,47 @@
-#litarchive.py
-import urllib.request,json, time, os, csv
+import urllib.request,json, time, os, csv, io
 from pprint import pprint
+import pandas as pd 
+
 
 apiUrl = 'https://api.pushshift.io/reddit/search'
 subreddit = 'nosleep'
-ourKeys = ('title', 'selftext')
 
 minScore = 5 #Minimum karma of posts we get &score>={minScore}.
 
 before = int(round(time.time()))
 ct = 0
 posts = []
-while True: 
-    url = f'{apiUrl}/submission/?before={before}&subreddit={subreddit}&size=20'
-    req = urllib.request.urlopen(url)
-    data =  json.loads(req.read())['data']
+with io.open('nosleep posts.txt', 'w+', encoding='utf-8') as f:
+    while True: 
+        url = f'{apiUrl}/submission/?subreddit={subreddit}&before={before}&size=50'
+        req = urllib.request.urlopen(url)
+        data =  json.loads(req.read())['data']
 
-    
-    for submission in data:
-        if 'title' in submission.keys() and 'selftext' in submission.keys():
-            if '&amp;#x200B;' in submission['selftext']:
-                text = submission['selftext'].replace('&amp;#x200B;', '')
-            else:
-                text = submission['selftext']
+        f.write('='*50 + '\n')
+        for submission in data:
+            if 'title' in submission.keys() and 'selftext' in submission.keys():
+                if submission['selftext']== '[removed]':
+                    continue
+                
+                title = submission['title'].strip()
+                
+                text = submission['selftext'].replace('&amp;#x200B','')
+                text = text.split('\n')
+                text = [i.strip() for i in text if i!=';']
 
-            text = text.strip()
-            
-            
-            posts.append((submission['title'], text))
-            ct = ct + 1
-            
-    break
-    before = data[-1]['created_utc']
+                text = '\n\t' + '\n\t'.join(text)
+        
+                
+                f.write(title + '\n\n')
+                f.write(submission['url']+ '\n\n')
+                f.write(text+ '\n')
+                f.write('='*50 + '\n')
+                
+             
+                posts.append([title, text])
+                ct = ct + 1
 
-print(len(posts))
-fname = 'nosleep posts' + str(time.time()) + '.csv'
-f = open(fname, mode='w+', newline='', encoding='utf-8-sig')
-    
-writer = csv.writer(f)
-    
-writer.writerow(['Title', 'Post Text'])
-for post in posts:
-    writer.writerow(post)
-f.close()
+        break
+        before = data[-1]['created_utc']
 
-os.startfile(fname)
-
-
+os.startfile('nosleep posts.txt')
